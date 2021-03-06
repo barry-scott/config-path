ed6a65c018e568d93a5cf0a7599da3b509805f65
Barry Scott <barry@barrys-emacs.org>
2021-03-06 10:52:54 +0000

    Update build and update scripts to use python3 rather the a specific version. python3 is usually the newest python installed. Add the twine check to catch errors in the package.

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-PY=python3.7
 rm -rf  build
 rm -rf  dist
 rm -rf  config-path.egg-info
 
-${PY} setup.py sdist bdist_wheel "$@"
-${PY} -m twine check dist/*
+python3 setup.py sdist bdist_wheel "$@"
+python3 -m twine check dist/*
 
 ls -1 dist/*.whl
